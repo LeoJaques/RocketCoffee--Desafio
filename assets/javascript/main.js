1a83a214ab1b472a944b99028438b445fcb6b1b7
Leonardo Jaques <leonardo.yan19@outlook.com>
2023-02-27 19:29:24 -0300

    ajuste overflow no menu-mobile

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,7 +3,7 @@ function openMenu() {
   const menu = document.querySelector('.nav_header-menu');
   const openButton = document.querySelector('.open-menu');
   const closeButton = document.querySelector('.close-menu');
-  document.body.classList.add('menu-expander');
+  document.body.classList.add('menu-expanded');
   
   // Alterna o display do menu para block
   menu.style.display = 'block';
@@ -14,9 +14,9 @@ function openMenu() {
 function closeMenu() {
   // Seleciona o menu e remove a classe .menu-expander do corpo
   var menu = document.querySelector('.nav_header-menu');
-  document.body.classList.remove('menu-expander');
   const openButton = document.querySelector('.open-menu');
   const closeButton = document.querySelector('.close-menu');
+  document.body.classList.remove('menu-expanded');
   
   // Alterna o display do menu para none
   menu.style.display = 'none';
